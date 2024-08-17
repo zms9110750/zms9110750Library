6bc47c9894f215cf0bbd98aabb54ec7a7dfa283f
1622989459@qq.com <1622989459@qq.com>
2024-08-17 09:31:37 +0800

    更新配方计算器

--- RecipeBalancing/Recipe.cs
+++ RecipeBalancing/Recipe.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,21 @@ namespace zms9110750Library.RecipeBalancing;
 /// <summary>
 /// 配方
 /// </summary>
-/// <param name="name">配方名</param>
-/// <param name="time">消耗时间</param>
-public record Recipe(string Name, TimeSpan Time)
+/// <param name="Name">配方名</param>
+/// <param name="Time">消耗时间(秒)</param>
+public sealed record Recipe(string Name, double Time)
 {
+	/// <summary>
+	/// 生产某个产品所需要的时间
+	/// </summary>
+	/// <param name="products">产品名</param>
+	/// <returns>平均产出每个产品所需时间</returns>
+	/// <remarks>如无法生产则返回<see cref="Timeout.InfiniteTimeSpan"/></remarks>
+	public double this[string products] => !Outputs.TryGetValue(products, out var value)
+		? double.PositiveInfinity
+		: value <= 0 ? throw new ArgumentException("该配方产出有0或负值")
+		: Time / value;
+
 	/// <summary>
 	/// 材料
 	/// </summary>
@@ -34,110 +46,28 @@ public record Recipe(string Name, TimeSpan Time)
 		StringBuilder sb = new StringBuilder(Name);
 		sb.Append(':', 2);
 		sb.Append('[');
-		sb.Append(Time);
+		sb.Append(TimeSpan.FromSeconds(Time));
 		sb.Append(']');
 		sb.Append('+');
 		sb.Append('[');
-		sb.AppendJoin(',', Inputs?.Select(ToString)!);
+		sb.AppendJoin(',', Inputs?.Select(ToString) ?? []);
 		sb.Append(']');
 
 		sb.Append('=', 2);
 		sb.Append('(');
-		sb.AppendJoin(',', Catalysts?.Select(ToString)!);
+		sb.AppendJoin(',', Catalysts?.Select(ToString) ?? []);
 		sb.Append(')');
 		sb.Append('=', 2);
 		sb.Append('>');
 
 		sb.Append('[');
-		sb.AppendJoin(',', Outputs?.Select(ToString)!);
+		sb.AppendJoin(',', Outputs?.Select(ToString) ?? []);
 		sb.Append(']');
 
 		return sb.ToString();
 	}
 
-	public static Recipe CraftingCalculator(IEnumerable<Recipe> recipes, Dictionary<string, double> finalProducts)
-	{
-		double Creat(string k, double arg)
-		{
-			return arg;
-		}
-		double Update(string k, double v, double arg)
-		{
-			return v + arg;
-		}
-
-		ArgumentNullException.ThrowIfNull(recipes);
-		ArgumentNullException.ThrowIfNull(finalProducts);
-
-		var recipeMap = recipes
-	   .SelectMany(recipe => recipe.Outputs, (recipe, output) => (output.Key, recipe))
-	   .GroupBy(x => x.Key, b => b.recipe)
-	   .ToDictionary(group => group.Key, group => group.First());
-
-
-		var time = recipeMap.Join(finalProducts, s => s.Key, v => v.Key
-		, (a, b) => a.Value.Time
-		).Max();
-
-		ConcurrentDictionary<string, double> 配方 = [];
-		ConcurrentDictionary<string, double> 所需 = new ConcurrentDictionary<string, double>(finalProducts);
-		ConcurrentDictionary<string, double> 亏欠 = [];
-		ConcurrentDictionary<string, double> 原料 = [];
-		ConcurrentDictionary<string, double> 多余 = new ConcurrentDictionary<string, double>(finalProducts);
-		while (!所需.IsEmpty)
-		{
-			foreach (var (k, v) in 所需)
-			{
-				if (recipeMap.TryGetValue(k, out var recipe) && recipe.Outputs.TryGetValue(k, out var output))
-				{
-					var scale = v / output;//计算配方产物数量与所需数量的比值。
-					配方.AddOrUpdate(recipe.Name, Creat, Update, recipe.Time / time * scale);
-					foreach (var item in recipe.Inputs ?? [])
-					{
-						亏欠.AddOrUpdate(item.Key, Creat, Update, scale * item.Value);
-					}
-					foreach (var item in recipe.Outputs)
-					{
-						if (item.Key != k)
-						{
-							多余.AddOrUpdate(item.Key, Creat, Update, scale * item.Value);
-						}
-					}
-				}
-				else
-				{
-					原料.AddOrUpdate(k, Creat, Update, v);
-				}
-			}
-			所需.Clear();
-			foreach (var (k, v) in 亏欠)
-			{
-				多余.Remove(k, out var more);
-				more -= v;
-				if (more < 0)
-				{
-					所需.AddOrUpdate(k, Creat, Update, -more);
-				}
-				else if (more > 0)
-				{
-					多余.AddOrUpdate(k, Creat, Update, more);
-				}
-			}
-			亏欠.Clear();
-		}
-
-		var name = $"[]";
-
-		var factory = new Recipe(name, time)
-		{
-			Inputs = new Dictionary<string, double>(原料),
-			Outputs = new Dictionary<string, double>(多余),
-			Catalysts = new Dictionary<string, double>(配方)
-		};
-		return factory;
-	}
-
-	static string ToString(KeyValuePair<string, double> pair)
+	public static string ToString(KeyValuePair<string, double> pair)
 	{
 		return pair.Value == 1 ? pair.Key : $"{pair.Value:0.##}*{pair.Key}";
 	}
---
+++ RecipeBalancing/RecipeSet.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+
+namespace zms9110750Library.RecipeBalancing;
+
+/// <summary>
+/// 配方集合
+/// </summary>
+public sealed record RecipeSet : ICollection<Recipe>, ILookup<string, Recipe>
+{
+	HashSet<Recipe> _recipes = [];
+	Lazy<ILookup<string, Recipe>> _lookup;
+
+#pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
+	public RecipeSet()
+#pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
+	{
+		UpdateLookup();
+	}
+
+	private void UpdateLookup()
+	{
+		if (_lookup is { IsValueCreated: false })
+		{
+			return;
+		}
+		_lookup = new Lazy<ILookup<string, Recipe>>(() => _recipes
+			.SelectMany(s => s.Outputs, (a, b) => (a, b))
+			.ToLookup(a => a.b.Key, b => b.a)
+		);
+	}
+
+	public override string ToString()
+	{
+		return string.Join('\n', _recipes);
+	}
+	public Recipe Balancer(IEnumerable<KeyValuePair<string, double>> finalProducts)
+	{
+		double Creat(string k, double arg)
+		{
+			return arg;
+		}
+		double Update(string k, double v, double arg)
+		{
+			return v + arg;
+		}
+
+		ArgumentNullException.ThrowIfNull(finalProducts);
+
+		var invalid = _lookup;
+
+		if (finalProducts is not IReadOnlyDictionary<string, double> products)
+		{
+			products = finalProducts.ToDictionary();
+		}
+
+
+		ConcurrentDictionary<string, double> 配方 = [];
+		ConcurrentDictionary<string, double> 所需 = [];
+		ConcurrentDictionary<string, double> 盈亏 = [];
+
+		double reference = products.Join((ILookup<string, Recipe>)this
+							   , s => s.Key
+							   , s => s.Key
+							   , (a, b) => MinTimeRecipe(b.Key)!.Time).Max();
+
+		foreach (var (product, quantity) in products)
+		{
+			if (MinTimeRecipe(product) is { } recipe)
+			{
+				var scale = reference / recipe[product];
+				var parallel = double.Ceiling(quantity / scale);
+				配方.AddOrUpdate(recipe.Name, Creat, Update, parallel);
+				foreach (var item in recipe.Inputs ?? [])
+				{
+					盈亏.AddOrUpdate(item.Key, Creat, Update, -item.Value * scale * parallel);
+				}
+				foreach (var item in recipe.Outputs ?? [])
+				{
+					盈亏.AddOrUpdate(item.Key, Creat, Update, item.Value * scale * parallel);
+				}
+			}
+			else
+			{
+				盈亏.AddOrUpdate(product, Creat, Update, -quantity);
+			}
+		}
+		do
+		{
+			所需.Clear();
+			foreach (var (product, quantity) in 盈亏)
+			{
+				if (quantity < 0 && Contains(product))
+				{
+					所需.AddOrUpdate(product, Creat, Update, products.GetValueOrDefault(product) - quantity);
+				}
+			}
+			foreach (var (product, quantity) in 所需)
+			{
+				if (MinTimeRecipe(product) is { } recipe)
+				{
+					配方.AddOrUpdate(recipe.Name, Creat, Update, quantity * recipe[product] / reference);
+					foreach (var item in recipe.Inputs ?? [])
+					{
+						盈亏.AddOrUpdate(item.Key, Creat, Update, -item.Value * quantity);
+					}
+					foreach (var item in recipe.Outputs ?? [])
+					{
+						盈亏.AddOrUpdate(item.Key, Creat, Update, item.Value * quantity);
+					}
+				}
+			}
+		} while (!所需.IsEmpty);
+
+		return _lookup != invalid
+			? throw new InvalidOperationException(" Collection was modified; enumeration operation may not execute.")
+			: new Recipe("[" + string.Join(',', finalProducts.Select(Recipe.ToString)) + "]", reference)
+			{
+				Inputs = new Dictionary<string, double>(盈亏.OrderBy(b => b.Value).TakeWhile(s => s.Value < 0).Select(s => KeyValuePair.Create(s.Key, -s.Value))),
+				Outputs = new Dictionary<string, double>(盈亏.OrderByDescending(b => b.Value).TakeWhile(s => s.Value > 0)),
+				Catalysts = new Dictionary<string, double>(配方.OrderByDescending(b => b.Value))
+			};
+	}
+
+	/// <summary>
+	/// 找到产出该产品的最短时间配方
+	/// </summary>
+	/// <param name="product">产品名</param>
+	/// <returns>配方</returns>
+	/// <remarks>若无记录能产出该产品的订单，则为null</remarks>
+	public Recipe? MinTimeRecipe(string product)
+	{
+		if (!Contains(product))
+		{
+			return null;
+		}
+		var a = this[product].MinBy(s => s[product]);
+		return double.IsPositiveInfinity(a?[product] ?? double.PositiveInfinity) ? null : a;
+	}
+
+	#region 接口 
+	public int Count => ((ICollection<Recipe>)_recipes).Count;
+	int ILookup<string, Recipe>.Count { get; }
+	bool ICollection<Recipe>.IsReadOnly => false;
+	public IEnumerable<Recipe> this[string key] => _lookup.Value[key];
+	public void Add(Recipe item)
+	{
+		((ICollection<Recipe>)_recipes).Add(item);
+		UpdateLookup();
+	}
+	public void Clear()
+	{
+		((ICollection<Recipe>)_recipes).Clear();
+		UpdateLookup();
+	}
+	public bool Contains(Recipe item) => ((ICollection<Recipe>)_recipes).Contains(item);
+	public void CopyTo(Recipe[] array, int arrayIndex) => ((ICollection<Recipe>)_recipes).CopyTo(array, arrayIndex);
+	public IEnumerator<Recipe> GetEnumerator() => ((IEnumerable<Recipe>)_recipes).GetEnumerator();
+	public bool Remove(Recipe item)
+	{
+		if (((ICollection<Recipe>)_recipes).Remove(item))
+		{
+			UpdateLookup();
+			return true;
+		}
+		return false;
+	}
+	IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_recipes).GetEnumerator();
+	public bool Contains(string key) => _lookup.Value.Contains(key);
+	IEnumerator<IGrouping<string, Recipe>> IEnumerable<IGrouping<string, Recipe>>.GetEnumerator() => _lookup.Value.GetEnumerator();
+	#endregion
+}
